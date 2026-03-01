fc83a12acfdeaf3b58ac66efb43e142c69a54edd
Justinxiaohao <1418661311@qq.com>
2026-03-01 15:31:28 +0800

    修复导入路径并更新依赖项以支持新的文本分割器

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_community.chat_models import ChatTongyi
-from langchain.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import tempfile
